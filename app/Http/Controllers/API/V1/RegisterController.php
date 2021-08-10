fe070eeaae8264cbfcc33e7a0bd01bf4272ffbd1
Eric <eric@pop-os.localdomain>
2021-08-10 17:21:39 -0400

    Validate register request

--- app/Http/Controllers/API/V1/RegisterController.php
+++ app/Http/Controllers/API/V1/RegisterController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\API\V1\RegisterRequest;
+use Illuminate\Http\JsonResponse;
 
 class RegisterController extends Controller
 {
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \App\Http\Controllers\API\V1\RegisterRequest  $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function __invoke(Request $request)
+    public function __invoke(RegisterRequest $request): JsonResponse
     {
         //
     }
