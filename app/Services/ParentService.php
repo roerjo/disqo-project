7554637d49441df51a6da66a81baa763e3a0e19a
Eric <eric@pop-os.localdomain>
2021-08-10 18:03:01 -0400

    Create user and send back API token

--- app/Services/ParentService.php
+++ app/Services/ParentService.php
@@ -2,6 +2,9 @@
 
 namespace App\Services;
 
+use Exception;
+use Illuminate\Support\Facades\Log;
+
 abstract class ParentService
 {
     private string $error = '';
@@ -13,9 +16,25 @@ abstract class ParentService
         return !empty($this->error);
     }
 
+    public function setError(string $message, Exception $e): void
+    {
+        Log::error($message, [
+            'message' => $e->getMessage(),
+            'file' => $e->getFile(),                
+            'line' => $e->getLine(),                
+        ]);
+
+        $this->error = $message;
+    }
+
     public function getError(): string
     {
         return $this->error;
+    }
+
+    public function setSuccess(array $successData): void
+    {
+        $this->success = $successData;
     }
 
     public function getSuccess(): array
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,12 +2,29 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Exception;
+
 class UserService extends ParentService
 {
     public function registerUser(array $userAttributes): self
     {
+        try {
+            $user = User::create($userAttributes);
 
+            $token = $user->createToken('api_token');
 
+            $this->setSuccess([
+                'token' => $token->plainTextToken,
+            ]);
+        } catch (Exception $e) {
+            $this->setError('Error while registering user', $e);
+            
+            return $this;
+        }
+
+        // Trigger UserRegistered event?
+        
         return $this;
     }
 }
